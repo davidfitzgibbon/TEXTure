137699b2539eb336845779abe7c01d296812fcf2
David Fitzgibbon <davidfitzgibbon@gmail.com>
2012-02-06 20:32:36 +0000

    Variables added

--- blur.js
+++ blur.js
@@ -1,4 +1,4 @@
-(function(){
+(function(type){
 
 	var v = "1.3.2";
 
@@ -19,38 +19,51 @@
 
 	function initMyBookmarklet() {
 		(window.myBookmarklet = function() {
-			
-			
-			var text_elements = new Array("p", "span", "h1", "h2", "h3", "h4", "h5", "h6", "a", "em", "b", "i");
 		
-			function img_blur(){
-				$('img').each(function(){
-					var img = $(this);
-					
-					var height = img.height();
-					var width = img.width();
-					
-					var src = '//placehold.it/'+width+'x'+height+'&text=%7C';
-					
-					img.attr('src', src);
-				});
+			function blur(type){
+			
+				var text_elements = new Array("p", "span", "h1", "h2", "h3", "h4", "h5", "h6", "a", "em", "b", "i");
+		
+				function img_blur(){
+					$('img').each(function(){
+						var img = $(this);
+						
+						var height = img.height();
+						var width = img.width();
+						
+						var src = '//placehold.it/'+width+'x'+height+'&text=%7C';
+						
+						img.attr('src', src);
+						img.css('box-shadow', '0 0 15px #ccc');
+						img.css('opacity', 0.5);				
+					});
+				}
+				
+				function text_blur(e){
+					$(e).each(function(){
+						var el = $(this);
+						var color = el.css('color');
+						
+						if(type == 'wire'){
+							el.css('background', color);
+						} else {
+							el.css('text-shadow', '0 0 15px '+color);
+						}
+	
+						el.css('opacity', 0.33);				
+					});
+				}
+				
+				for(el in text_elements){
+					text_blur(text_elements[el]);
+				}
+				
+				img_blur();
+				
+			
 			}
 			
-			function text_blur(e){
-				$(e).each(function(){
-					var el = $(this);
-					var color = el.css('color');
-					
-					
-					el.css('background', color);
-				});
-			}
 			
-			for(el in text_elements){
-				text_blur(text_elements[el]);
-			}
-			
-			img_blur();
 			
 			
 		})();
