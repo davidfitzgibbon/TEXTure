21f81edfa64a33a92539c0ff99c30510bb1aeef5
David Fitzgibbon <davidfitzgibbon@gmail.com>
2012-02-17 09:18:02 +0000

    Getting pages working with full code

---
+++ texture.js
@@ -0,0 +1,61 @@
+(function(type){
+
+	var v = "1.3.2";
+
+	if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+		var done = false;
+		var script = document.createElement("script");
+		script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+		script.onload = script.onreadystatechange = function(){
+			if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+				done = true;
+				initMyBookmarklet();
+			}
+		};
+		document.getElementsByTagName("head")[0].appendChild(script);
+	} else {
+		initMyBookmarklet();
+	}
+
+	function initMyBookmarklet() {
+		(window.myBookmarklet = function() {
+		
+			var text_elements = new Array('html', 'body', 'div', 'span', 'applet', 'object', 'iframe', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'blockquote', 'pre', 'a', 'abbr', 'acronym', 'address', 'big', 'cite', 'code', 'del', 'dfn', 'em', 'ins', 'kbd', 'q', 's', 'samp', 'small', 'strike', 'strong', 'sub', 'sup', 'tt', 'var', 'b', 'u', 'i', 'center', 'dl', 'dt', 'dd', 'ol', 'ul', 'li', 'fieldset', 'form', 'label', 'legend', 'table', 'caption', 'tbody', 'tfoot', 'thead', 'tr', 'th', 'td', 'article', 'aside', 'canvas', 'details', 'embed', 'figure', 'figcaption', 'footer', 'header', 'hgroup', 'menu', 'nav', 'output', 'ruby', 'section', 'summary', 'time', 'mark');
+		
+			function img_blur(){
+				$('img').each(function(){
+					var img = $(this);
+					
+					var height = img.height();
+					var width = img.width();
+					
+					var src = '//placehold.it/'+width+'x'+height+'&text=%7C';
+					
+					img.attr('src', src);
+					img.css('box-shadow', '0 0 15px #ccc');
+					img.css('opacity', 0.5);				
+				});
+			}
+			
+			function text_blur(e){
+				$(e).each(function(){
+					var el = $(this);
+					var color = el.css('color');
+
+					el.css('text-shadow', '0 0 15px '+color+',0 0 15px '+color+',0 0 15px '+color+',0 0 15px '+color+',0 0 15px '+color+',0 0 15px '+color+',0 0 15px '+color+',0 0 15px '+color+',0 0 15px '+color);	
+				});
+			}
+			
+			for(el in text_elements){
+				text_blur(text_elements[el]);
+			}
+			
+			img_blur();
+			
+			
+			
+			
+		})();
+	}
+
+})();
