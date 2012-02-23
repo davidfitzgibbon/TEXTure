98ac752c99feb5c5fe10258964f915b1b2964365
David Fitzgibbon <davidfitzgibbon@gmail.com>
2012-02-23 20:32:17 +0000

    Improving styles

--- texture.js
+++ texture.js
@@ -20,7 +20,7 @@
 	function initMyBookmarklet() {
 		(window.myBookmarklet = function() {
 		
-			var text_elements = new Array('span', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'blockquote', 'pre', 'a', 'abbr', 'acronym', 'address', 'big', 'cite', 'code', 'del', 'dfn', 'em', 'ins', 'kbd', 'q', 's', 'samp', 'small', 'strike', 'strong', 'sub', 'sup', 'tt', 'var', 'b', 'u', 'i', 'center', 'dl', 'dt', 'dd', 'ol', 'li', 'label', 'legend', 'caption', 'th', 'td', 'time', 'mark');
+			var text_elements = new Array('figcaption', 'span', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'blockquote', 'pre', 'a', 'abbr', 'acronym', 'address', 'big', 'cite', 'code', 'del', 'dfn', 'em', 'ins', 'kbd', 'q', 's', 'samp', 'small', 'strike', 'strong', 'sub', 'sup', 'tt', 'var', 'b', 'u', 'i', 'center', 'dl', 'dt', 'dd', 'ol', 'li', 'label', 'legend', 'caption', 'th', 'td', 'time', 'mark');
 		
 			function img_blur(){
 				$('img').each(function(){
